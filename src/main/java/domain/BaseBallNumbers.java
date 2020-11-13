31dfde002e1f66195693d801d894bc7b783fe934
Yongwoo, Lee <leeyw910205@gmail.com>
2020-11-13 14:46:48 +0900

    feat: BaseBallNumbers 생성로직 작성 및 예외로직 처리

---
+++ src/main/java/domain/BaseBallNumbers.java
@@ -0,0 +1,48 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BaseBallNumbers {
+
+    public static final int PICK_NUM_SIZE = 3;
+    private List<BaseBallNumber> baseBallNumbers;
+
+
+    private BaseBallNumbers(final List<BaseBallNumber> baseBallNumbers) {
+        this.baseBallNumbers = baseBallNumbers;
+    }
+
+    public static BaseBallNumbers of(final List<Integer> numbers) {
+        validation(numbers);
+        return new BaseBallNumbers(convertBaseBallNumber(numbers));
+    }
+
+    private static void validation(final List<Integer> numbers) {
+        if(numbers == null || numbers.size() != PICK_NUM_SIZE) {
+            throw new IllegalArgumentException("정확히 3개 숫자만 선택 할수 있습니다.");
+        }
+        duplicationCheck(numbers);
+    }
+
+    private static void duplicationCheck(final List<Integer> numbers) {
+        Set<Integer> notDuplicated = new HashSet<>(numbers);
+        if(notDuplicated.size() < PICK_NUM_SIZE) {
+            throw new IllegalArgumentException("중복된 숫자는 들어갈수 없습니다");
+        }
+    }
+
+    private static List<BaseBallNumber> convertBaseBallNumber(final List<Integer> numbers) {
+        List<BaseBallNumber> baseBallNumbers = new ArrayList<>();
+        for(int i=0; i<numbers.size(); i++) {
+            baseBallNumbers.add(BaseBallNumber.of(i+1, numbers.get(i)));
+        }
+        return baseBallNumbers;
+    }
+
+    public List<BaseBallNumber> getBaseBallNumbers() {
+        return baseBallNumbers;
+    }
+}
